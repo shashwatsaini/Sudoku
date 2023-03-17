1b98f2890ab18537b8bdd34b5e270735ff0bb17f
Shashwat Saini <125039068+shashwatsaini@users.noreply.github.com>
2023-03-17 13:43:36 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,13 +1,22 @@
 import pygame
 import main
 import sys
+import data
+import pandas as pd
 
 pygame.init()
 objects = []
 run=True
-screen= pygame.display.set_mode((620, 200))
+screen= pygame.display.set_mode((620, 220))
+icon= pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
 font = pygame.font.Font('freesansbold.ttf', 32)
 font2 = pygame.font.Font('freesansbold.ttf', 26)
+bg= pygame.image.load('bg.jpg').convert_alpha()
+bg= pygame.transform.scale(bg, (620,220))
+bg.set_alpha(40)
+screen.blit(bg, (0,0))
+data.init()
 
 #Creates a class for buttons
 class Button():
@@ -77,10 +86,76 @@ def callMain(n):
         main.getboard(0,2)
         main.gameloop()
 
+def display_highscore2():
+    screen_highscore=pygame.display.set_mode((300,600))
+    temp= font.render('Highscores:', True, (255,0,0))
+    run=True
+    max_list=pd.DataFrame()
+    max_list= data.highscore()
+    #max= max_list.iloc[0]
+    max_str_temp= max_list.to_string(index=False)
+    max_str_final=''
+    words=max_str_temp.split()
+    for i in words:
+        if i=='taken':
+            max_str_final+=i+'\n'
+        else:
+            max_str_final+=i+' '
+
+    #print(max)
+    print(max_str_final)
+    font3= pygame.font.Font('freesansbold.ttf', 10)
+    temp_max_str=font3.render(max_str_final, True, (255,0,0))
+    while run:
+        screen_highscore.fill((255,255,255))
+        screen_highscore.blit(temp,(60,90))
+        screen_highscore.blit(temp_max_str,(20,130))
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+                screen_highscore=pygame.display.set_mode((620,220))
+        pygame.display.update()
+def display_highscore():
+    screen_highscore=pygame.display.set_mode((300,600))
+    temp= font.render('Highscores:', True, (255,0,0))
+    run=True
+    max_list=data.highscore()
+    #print(max_list)
+    font3= pygame.font.Font('freesansbold.ttf', 14)
+    max_list_str=[]
+    count=0
+    i=0
+    for x in max_list:
+        if count==3:
+            i+=1
+            count=0
+        if count==0:
+            max_list_str.append('')
+        max_list_str[i]+=str(x)
+        max_list_str[i]+='      '
+        count+=1
+    col_str='Difficulty  Date Played  Time Taken'
+    col_blit=font3.render(col_str, True, (255,0,0))
+    print(max_list_str)
+    while run: 
+        screen_highscore.fill((255,255,255))
+        screen_highscore.blit(temp,(60,90))
+        screen_highscore.blit(col_blit, (30, 130))
+        #Columns:
+        for x in range(len(max_list_str)):
+            score_blit=font3.render(max_list_str[x], True, (255,0,0))
+            screen_highscore.blit(score_blit,(80,150+(x*20)))
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+                screen_highscore=pygame.display.set_mode((620,220))
+        pygame.display.update()
+
 customButton = Button(180, 60, 80, 30, '1', lambda: callMain(1))
 customButton = Button(280, 60, 80, 30, '2', lambda: callMain(2))
 customButton= Button(380, 60, 80, 30, '3', lambda: callMain(3))
 customButtton= Button(280, 140, 80, 30, 'Load', lambda: callMain(4))
+customButton= Button(235, 180, 180, 30, 'Highscore', lambda: display_highscore())
 
 def drawbg():
     screen.fill((255,255,255))
@@ -100,4 +175,5 @@ while run:
             run=False
     for object in objects:
         object.process()
+    screen.blit(bg, (0,0))
     pygame.display.update()
